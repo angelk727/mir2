2eaa5acada93212b34efdeb37afcf95cca5694c3
angelk727 <zhq0388@hotmail.com>
2024-04-06 03:13:13 +0800

    CityGate修改

--- Server/MirObjects/Monsters/CityGate.cs
+++ Server/MirObjects/Monsters/CityGate.cs
@@ -57,7 +57,7 @@ namespace Server.MirObjects.Monsters
                         new Point(0, -1),
                         new Point(-1, 1),
                         new Point(-1, 0),
-                        new Point(-2, 1),
+                        new Point(-1, 2),
                     };
                     break;
             }
