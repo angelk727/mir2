3bb8855d3897e9205ba67bd33ada614582fb16d2
fanglei <fanglei@svolt.cn>
2025-01-08 19:03:49 +0800

    调整灵宠到水晶发包

--- Shared/Data/IntelligentCreatureData.cs
+++ Shared/Data/IntelligentCreatureData.cs
@@ -10,9 +10,6 @@
     public int SemiAutoPickupRange = 0;
 
     public bool CanProduceBlackStone = false;
-    public string Info = "";
-    public string Info1 = "";
-    public string Info2 = "";
 
     public IntelligentCreatureRules()
     {
@@ -29,10 +26,6 @@
         SemiAutoPickupRange = reader.ReadInt32();
 
         CanProduceBlackStone = reader.ReadBoolean();
-
-        Info = reader.ReadString();
-        Info1 = reader.ReadString();
-        Info2 = reader.ReadString();
     }
 
     public void Save(BinaryWriter writer)
@@ -46,9 +39,6 @@
         writer.Write(SemiAutoPickupRange);
 
         writer.Write(CanProduceBlackStone);
-        writer.Write(Info);
-        writer.Write(Info1);
-        writer.Write(Info2);
     }
 }
 
