e7c86b0615cad91e57beea8122252ab048f34f98
zhutian <1033566918@qq.com>
2025-01-17 21:05:40 +0800

    添加补丁资源配置工具

--- AutoPatche/Form1.Designer.cs
+++ AutoPatche/Form1.Designer.cs
@@ -28,31 +28,276 @@
         /// </summary>
         private void InitializeComponent()
         {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
             button1 = new Button();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            button3 = new Button();
+            flowLayoutPanel4 = new FlowLayoutPanel();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            panel1 = new Panel();
+            textBox5 = new TextBox();
+            label4 = new Label();
+            panel2 = new Panel();
+            textBox6 = new TextBox();
+            label5 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            textBox2 = new TextBox();
+            flowLayoutPanel1.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
+            flowLayoutPanel4.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel2);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel3);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel4);
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(panel3);
+            flowLayoutPanel1.Controls.Add(panel4);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(622, 382);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(label1);
+            flowLayoutPanel2.Controls.Add(textBox1);
+            flowLayoutPanel2.Controls.Add(button2);
+            flowLayoutPanel2.Controls.Add(button1);
+            flowLayoutPanel2.Location = new Point(3, 3);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(612, 31);
+            flowLayoutPanel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 17);
+            label1.TabIndex = 0;
+            label1.Text = "目录：";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(109, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(285, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(400, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "选择目录";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(362, 8);
+            button1.Location = new Point(481, 3);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "生成";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.Controls.Add(label2);
+            flowLayoutPanel3.Controls.Add(textBox3);
+            flowLayoutPanel3.Controls.Add(button3);
+            flowLayoutPanel3.Location = new Point(3, 40);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(612, 30);
+            flowLayoutPanel3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 17);
+            label2.TabIndex = 0;
+            label2.Text = "验证文件输出目录";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(109, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(285, 23);
+            textBox3.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(400, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "选择目录";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // flowLayoutPanel4
+            // 
+            flowLayoutPanel4.Controls.Add(label3);
+            flowLayoutPanel4.Controls.Add(textBox4);
+            flowLayoutPanel4.Location = new Point(3, 76);
+            flowLayoutPanel4.Name = "flowLayoutPanel4";
+            flowLayoutPanel4.Size = new Size(607, 36);
+            flowLayoutPanel4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.Location = new Point(3, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 23);
+            label3.TabIndex = 0;
+            label3.Text = "下载文件目录地址";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(109, 3);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(286, 23);
+            textBox4.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label4);
+            panel1.Location = new Point(3, 118);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(607, 29);
+            panel1.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(109, 0);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(285, 23);
+            textBox5.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.Location = new Point(3, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 23);
+            label4.TabIndex = 0;
+            label4.Text = "验证文件名";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(label5);
+            panel2.Location = new Point(3, 153);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(607, 29);
+            panel2.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(109, 3);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(285, 23);
+            textBox6.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.Location = new Point(3, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 23);
+            label5.TabIndex = 0;
+            label5.Text = "版本号";
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(3, 188);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(607, 32);
+            panel3.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(textBox2);
+            panel4.Location = new Point(3, 226);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(607, 144);
+            panel4.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(3, 3);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(604, 128);
+            textBox2.TabIndex = 0;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(622, 382);
+            Controls.Add(flowLayoutPanel1);
+            Margin = new Padding(2, 3, 2, 3);
             Name = "Form1";
             Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel2.PerformLayout();
+            flowLayoutPanel3.ResumeLayout(false);
+            flowLayoutPanel3.PerformLayout();
+            flowLayoutPanel4.ResumeLayout(false);
+            flowLayoutPanel4.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
+        private FlowLayoutPanel flowLayoutPanel1;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private TextBox textBox1;
         private Button button1;
+        private TextBox textBox2;
+        private Button button2;
+        private Label label2;
+        private TextBox textBox3;
+        private FlowLayoutPanel flowLayoutPanel4;
+        private Label label3;
+        private TextBox textBox4;
+        private Panel panel1;
+        private Button button3;
+        private Panel panel2;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private Panel panel3;
+        private TextBox textBox6;
+        private Panel panel4;
     }
 }
--- AutoPatche/Form1.cs
+++ AutoPatche/Form1.cs
@@ -1,10 +1,245 @@
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
 namespace AutoPatche
 {
     public partial class Form1 : Form
     {
+        TextBox RemarkContent,
+            OutDirPath,
+            IPAddrContent,
+            DirPathConten,
+            VersionNO;
+
         public Form1()
         {
             InitializeComponent();
+            RemarkContent = textBox2;
+            OutDirPath = textBox3;
+            IPAddrContent = textBox4;
+            DirPathConten = textBox5;
+            VersionNO = textBox6;
+            LoadSettings();
+        }
+
+        string DirPath = string.Empty;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string FILE = SelectDirectory();
+            if (string.IsNullOrEmpty(FILE))
+            {
+                MessageBox.Show("目录路径不存在，请选择有效的目录路径");
+                return;
+            }
+            textBox1.Text = DirPath = FILE;
+        }
+
+        private string SelectDirectory()
+        {
+            string FILE = string.Empty;
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FILE = folderBrowserDialog.SelectedPath;
+                    MessageBox.Show($"Selected Directory: {DirPath}");
+                }
+            }
+            return FILE;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(DirPath))
+            {
+                GenerateDirectoryJson();
+            }
+            else
+            {
+                MessageBox.Show("目录路径不存在，请选择有效的目录路径");
+            }
+        }
+
+        private void GenerateDirectoryJson()
+        {
+            if (string.IsNullOrEmpty(DirPath))
+            {
+                MessageBox.Show("未选择任何目录");
+                return;
+            }
+
+            if (!ValidateOutputDirectory(OutDirPath.Text))
+            {
+                MessageBox.Show("输出目录无效，请输入有效的输出目录路径");
+                return;
+            }
+
+            //if (!ValidateIPAddress(IPAddrContent.Text))
+            //{
+            //    MessageBox.Show("IP地址无效，请输入有效的IP地址");
+            //    return;
+            //}
+
+            if (!ValidateVersion(VersionNO.Text))
+            {
+                MessageBox.Show("版本号无效，请输入有效的版本号");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(DirPathConten.Text))
+            {
+                MessageBox.Show("目录路径无效，请输入有效的目录路径");
+                return;
+            }
+            RemarkContent.Text=string.Empty;
+            string outputPath = DirPathConten.Text; // 替换为你的输出路径
+            string outputDirectory = OutDirPath.Text;
+            if (!Directory.Exists(outputDirectory))
+            {
+                Directory.CreateDirectory(outputDirectory);
+            }
+            outputPath = Path.Combine(outputDirectory, outputPath);
+            var assets = new Dictionary<string, Asset>();
+            var files = Directory.GetFiles(DirPath, "*", SearchOption.AllDirectories);
+            int totalFiles = files.Length;
+            ProcessDirectory(DirPath, totalFiles);
+
+            void ProcessDirectory(string currentDirectory, int totalFiles)
+            {
+                var files = Directory.GetFiles(currentDirectory, "*", SearchOption.TopDirectoryOnly);
+                var directories = Directory.GetDirectories(currentDirectory, "*", SearchOption.TopDirectoryOnly);
+
+                foreach (var filePath in files)
+                {
+                    if (File.Exists(filePath))
+                    {
+                        string relativePath = filePath.Replace(DirPath, "").Replace("\\", "/").TrimStart('/');
+                        string md5 = CalculateMD5(filePath);
+                        long size = new FileInfo(filePath).Length;
+
+                        assets[relativePath] = new Asset { md5 = md5, size = size };
+                    }
+                }
+
+                foreach (var directory in directories)
+                {
+                    ProcessDirectory(directory, totalFiles);
+                }
+
+                // 更新进度条
+                RemarkContent.Text += $"正在处理目录: {currentDirectory} ({assets.Count}/{totalFiles})" + Environment.NewLine;
+            }
+
+            var root = new Root
+            {
+                assets = assets,
+                packageUrl = IPAddrContent.Text,
+                VersionUrl = IPAddrContent.Text + "/" + DirPathConten.Text,
+                version = VersionNO.Text
+            };
+
+            string json = JsonConvert.SerializeObject(root, Formatting.Indented);
+            File.WriteAllText(outputPath, json);
+            // 清除进度条
+            RemarkContent.Text += "目录JSON生成完毕: " + outputPath + Environment.NewLine;
+            MessageBox.Show("目录JSON生成完毕: " + outputPath);
+        }
+
+        private static string CalculateMD5(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return System.BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        private bool ValidateOutputDirectory(string path)
+        {
+            return Directory.Exists(Path.GetDirectoryName(path));
+        }
+
+        private bool ValidateIPAddress(string ipAddress)
+        {
+            string pattern = @"^(http:\/\/|https:\/\/)?([a-zA-Z0-9\-\.]+)(:\d+)?(\/)?$";
+            return Regex.IsMatch(ipAddress, pattern);
+        }
+
+        private bool ValidateVersion(string version)
+        {
+            string pattern = @"^\d+\.\d+\.\d+\.\d+$";
+            return Regex.IsMatch(version, pattern);
+        }
+
+        private void LoadSettings()
+        {
+            if (File.Exists("settings.json"))
+            {
+                var settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText("settings.json"));
+                RemarkContent.Text = settings.RemarkContent;
+                OutDirPath.Text = settings.OutDirPath;
+                IPAddrContent.Text = settings.IPAddrContent;
+                DirPathConten.Text = settings.DirPathConten;
+                VersionNO.Text = settings.VersionNO;
+            }
+        }
+
+        private void SaveSettings()
+        {
+            var settings = new Settings
+            {
+                RemarkContent = RemarkContent.Text,
+                OutDirPath = OutDirPath.Text,
+                IPAddrContent = IPAddrContent.Text,
+                DirPathConten = DirPathConten.Text,
+                VersionNO = VersionNO.Text
+            };
+            File.WriteAllText("settings.json", JsonConvert.SerializeObject(settings, Formatting.Indented));
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            SaveSettings();
+            base.OnFormClosing(e);
+        }
+
+        public class Asset
+        {
+            public string md5 { get; set; }
+            public long size { get; set; }
+        }
+
+        public class Root
+        {
+            public Dictionary<string, Asset> assets { get; set; }
+            public string packageUrl { get; set; }
+            public string VersionUrl { get; set; }
+            public string version { get; set; }
+        }
+
+        public class Settings
+        {
+            public string RemarkContent { get; set; }
+            public string OutDirPath { get; set; }
+            public string IPAddrContent { get; set; }
+            public string DirPathConten { get; set; }
+            public string VersionNO { get; set; }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string FILE = SelectDirectory();
+            if (string.IsNullOrEmpty(FILE))
+            {
+                MessageBox.Show("目录路径不存在，请选择有效的目录路径");
+                return;
+            }
+            OutDirPath.Text = FILE;
         }
     }
 }
